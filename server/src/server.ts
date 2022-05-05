6776347f53ce2f58bddba636e0f427ef161895f4
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2022-05-05 16:39:02 -0300

    criação de um feedback no banco.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,22 @@
 import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
+
+app.use(express.json());
+// continuar a partir de 47 min
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const data = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+  return res.status(201).json({ data });
+});
 
 app.listen(3333, () => {
   console.log("Serviço rodando...");
